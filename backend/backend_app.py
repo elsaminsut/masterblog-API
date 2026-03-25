d4086b2faaac6e16becac70f0b51e897eedaaac7
Elsa Minsut <elsaminsut@gmail.com>
2026-03-25 13:11:22 +0100

    feat: add delete post by id

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -9,6 +9,12 @@ POSTS = [
     {"id": 2, "title": "Second post", "content": "This is the second post."},
 ]
 
+def find_post_by_id(id):
+    if isinstance(id, int):
+        post = next((post for post in POSTS if post["id"] == id), None)
+        return post
+    return jsonify({"error": f"invalid type for post id: {id}"}), 400
+
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
@@ -20,7 +26,7 @@ def add_post():
 
     if not data.get("title") and not data.get("content"):
         return jsonify({"error": "body fields cannot be empty"}), 400
-    
+
     if not data.get("title"):
         return jsonify({"error": "title field cannot be empty"}), 400
 
@@ -37,6 +43,13 @@ def add_post():
     POSTS.append(new_post)
     return jsonify(POSTS), 201
 
+@app.route('/api/posts/<int:post_id>', methods=['DELETE'])
+def delete_post(post_id):
+    post = find_post_by_id(post_id)
+    if not post:
+        return jsonify({"error": f"post with id {post_id} not found"}), 404
+    POSTS.remove(post)
+    return jsonify({"message": f"Post with id {post_id} has been deleted successfully."}), 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
